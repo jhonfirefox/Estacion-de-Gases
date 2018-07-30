8a1a02aa483fa1f35bdbfd1a5048877cd21fc6c9
jhonfirefox <39972493+jhonfirefox@users.noreply.github.com>
2018-07-30 11:31:49 -0500

    Create letreros.h

---
+++ letreros.h
@@ -0,0 +1,23 @@
+//Archivo de cabecera para almacenar los letreros en la memoria EEPROM
+#ifndef LETREROS_H
+#define LETREROS_H
+
+#define EMPRESA "BioGreenPlants"
+#define LETRERO1 "Memoria INI"
+#define LETRERO2 "ERROR-M.SD"
+#define LETRERO3 "1 segundo"
+#define LETRERO4 "1 Minuto"
+#define LETRERO5 "10 Minutos"
+#define LETRERO6 "30 Minutos"
+#define LETRERO7 "60 Minutos"
+#define MENU1 "Con. Fecha-Hora"
+#define MENU2 "Ej. Programa"
+#define MENU3 "Con. T. Medicion"
+//#define MENU4 "Con. Bluetooth"
+#define MENU4 "Hab. Programa"
+//#define MENU5 "Env. Datos"
+//#define MENU6 "Borra-SD"
+//#define MENU7 "Calibrar-Sensor."
+#define MENU5 "Salir..."
+
+#endif
